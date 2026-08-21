eb35c46661ddad7bf80ae2fc4d62d608a9ed8548
Perry Moerland <p.d.moerland@amsterdamumc.nl>
2026-08-21 12:39:26 +0200

    Changes to setup files and adding clean-surf files

--- setup/generative_models_setup.sh
+++ setup/generative_models_setup.sh
@@ -3,79 +3,66 @@
 # Install miniconda3 in /opt for all users
 # https://servicedesk.surf.nl/wiki/spaces/WIKI/pages/30668784/Manual+use+of+miniconda
 # https://askubuntu.com/questions/1508169/how-can-i-install-miniconda-to-be-accessible-to-all-users
-	cd /opt
-	# Download the installation script
-	sudo wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-	# Run the installation script: "-b" skips all confirmations with "yes"
-	sudo bash Miniconda3-latest-Linux-x86_64.sh -b -u -p miniconda3
-	# Accept the terms of service (tos) for all used channels.
-	sudo /opt/miniconda3/bin/conda tos accept
-	# Update conda
-	sudo /opt/miniconda3/bin/conda update conda -y
+cd /opt
+# Download the installation script
+sudo wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# Run the installation script: "-b" skips all confirmations with "yes"
+sudo bash Miniconda3-latest-Linux-x86_64.sh -b -u -p miniconda3
+# Accept the terms of service (tos) for all used channels.
+sudo /opt/miniconda3/bin/conda tos accept
+# Update conda
+sudo /opt/miniconda3/bin/conda update conda -y
 
-	# Unpack environment into directory `my_env`
-	sudo mkdir /data/generative_models
-	cd /data/generative_models
-	sudo mkdir -p esmfold
-    sudo cp /home/pmoerland/researchdrive/Research_Drive_Perry_Moerland\ \(Projectfolder\)/BioSB_summer_school/Generative_models/conda-envs-amelia/esmfold.tar.gz .	
-	sudo tar -xzf esmfold.tar.gz -C esmfold
-	sudo mkdir -p protein-design
-	sudo cp /home/pmoerland/researchdrive/Research_Drive_Perry_Moerland\ \(Projectfolder\)/BioSB_summer_school/Generative_models/conda-envs-amelia/protein-design.tar.gz .	
-	sudo tar -xzf protein-design.tar.gz -C protein-design
+# Unpack environment into directory `my_env`
+sudo mkdir /data/generative_models
+cd /data/generative_models
+sudo mkdir -p esmfold
+sudo cp /home/pmoerland/researchdrive/Research_Drive_Perry_Moerland\ \(Projectfolder\)/BioSB_summer_school/Generative_models/conda-envs-amelia/esmfold.tar.gz .	
+sudo tar -xzf esmfold.tar.gz -C esmfold
+sudo mkdir -p protein-design
+sudo cp /home/pmoerland/researchdrive/Research_Drive_Perry_Moerland\ \(Projectfolder\)/BioSB_summer_school/Generative_models/conda-envs-amelia/protein-design.tar.gz .	
+sudo tar -xzf protein-design.tar.gz -C protein-design
+sudo rm esmfold.tar.gz protein-design.tar.gz
 
 # Then, move environments to /opt/miniconda3/envs
-	sudo mv protein-design esmfold /opt/miniconda3/envs/
-
-# Initialize conda
-	miniconda3/bin/conda init
-	# Close the current terminal and start a new one.
-	# Try the version parameter
-	conda --version
+sudo mv protein-design esmfold /opt/miniconda3/envs/
 
 # Git clone RFdiffusion, ProteinMPNN, ESMFold in /data/generative_models
-	cd /data/generative_models
-	if [[ ! -d "RFdiffusion" ]]; then 
-		sudo git clone https://github.com/RosettaCommons/RFdiffusion.git
-	fi
-	if [[ ! -d "RFdiffusion/models" ]]; then
-		./RFdiffusion/scripts/download_models.sh RFdiffusion/models
-	fi
-	if [[ ! -d "ProteinMPNN" ]]; then
-		sudo git clone https://github.com/dauparas/ProteinMPNN.git
-	fi
-	if [[ ! -d "ESMFold" ]]; then
-		sudo git clone https://github.com/sara-nl/ESMFold_Snellius ESMFold
-	fi
+cd /data/generative_models
+if [[ ! -d "RFdiffusion" ]]; then 
+	sudo git clone https://github.com/RosettaCommons/RFdiffusion.git
+fi
+if [[ ! -d "RFdiffusion/models" ]]; then
+	sudo chmod u+x ./RFdiffusion/scripts/download_models.sh;
+	sudo ./RFdiffusion/scripts/download_models.sh RFdiffusion/models
+fi
+if [[ ! -d "ProteinMPNN" ]]; then
+	sudo git clone https://github.com/dauparas/ProteinMPNN.git
+fi
+if [[ ! -d "ESMFold" ]]; then
+	sudo git clone https://github.com/sara-nl/ESMFold_Snellius ESMFold
+fi
+
+# Initialize conda
+/opt/miniconda3/bin/conda init
+# Close the current terminal and start a new one.
+# Try the version parameter
+conda --version
 
 # Pip install RFdiffusion in protein-design environment
-	conda activate protein-design
-    sudo pip install -e "/data/generative_models/RFdiffusion/env/SE3Transformer"
-   	sudo pip install -e "/data/generative_models/RFdiffusion"
+conda activate protein-design
+sudo pip install -e "/data/generative_models/RFdiffusion/env/SE3Transformer"
+sudo pip install -e "/data/generative_models/RFdiffusion"
 
 # Problem with DGL and cuda (while running RFdiffusion):
 # `OSError: libcusparse.so.11: cannot open shared object file: No such file or directory`
 # Fix install cuda
-	conda activate protein-design
-    sudo /opt/miniconda3/bin/conda install -y -c conda-forge cudatoolkit=11.6 cudnn=8.4
-    sudo /opt/miniconda3/bin/conda env config vars set LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+sudo /opt/miniconda3/bin/conda install -y -c conda-forge cudatoolkit=11.6 cudnn=8.4
+sudo /opt/miniconda3/bin/conda env config vars set LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 # Reactivate env
-	conda deactivate
-	conda activate protein-design
+conda deactivate
+conda activate protein-design
+conda deactivate
 
 # Change kernel.json to connect Jupyter to local environment by default
-	sudo nano /usr/local/share/jupyter/kernels/src-default/kernel.json
-
-{
- "argv": [
-  "/opt/miniconda3/envs/protein-design/bin/python",
-  "-m",
-  "ipykernel_launcher",
-  "-f",
-  "{connection_file}"
- ],
- "display_name": "Python (protein-design via src-default)",
- "language": "python",
- "metadata": {
-  "debugger": true
- }
-}
+sudo cp /home/pmoerland/researchdrive/Research_Drive_Perry_Moerland\ \(Projectfolder\)/BioSB_summer_school/Generative_models/conda-envs-amelia/kernel.json /usr/local/share/jupyter/kernels/src-default/kernel.json
---
+++ setup/generative_models_test.sh
@@ -0,0 +1,9 @@
+# Initialize conda
+/opt/miniconda3/bin/conda init
+# Close the current terminal and start a new one.
+# Try the version parameter
+conda --version
+
+git clone https://github.com/BioSB-courses/BioSB-SummerSchool-2026.git
+cp -r BioSB-SummerSchool-2026/Practical-2-Generative-models/* .
+
